9965efa4c11cc0e31cfba2d1457578fc4ed25426
Ken Usa <kusa_230000002006@uic.edu.ph>
2025-01-16 15:53:59 +0800

    Delete Usa_AT#2.sql

--- Usa_AT#2.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE therapists (
-    Therapist_Number INT(4) AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    Patient_Number INT(4) NOT NULL,
-    Therapist_Name VARCHAR(100) NOT NULL,
-    Therapist_Address VARCHAR(200),
-    CONSTRAINT fk_patient FOREIGN KEY (Patient_Number) REFERENCES patients (Patient_Number) ON DELETE CASCADE
-);
